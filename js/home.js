bf3239535237d4b3a64eec5aefb3bf61071e9dac
jieery <lhshun2012@163.com>
2018-04-13 21:42:35 +0800

    home ceshi

--- js/home.js
+++ js/home.js
@@ -1,24 +1,3 @@
-$(document).ready(function(){
-	var banner = new Swiper('#ban', {
-	    autoplay: 4000,
-	    speed:800,
-	    autoplayDisableOnInteraction : false,
-	    pagination : '.swiper-pagination',
-	    paginationClickable :true,
-	    lazyLoading : true,
-		lazyLoadingInPrevNext : true,
-		loop : true,
-	});
-	var teachers = new Swiper('.teachers-banner-container', {
-	    speed:800,
-	    pagination : '.swiper-pagination',
-	    paginationClickable :true,
-	});
-	$('.teachers-banner li').hover(function(){
-		$(this).find('.main').slideToggle(200);
-	});
-	$('#my-video').width(100+'%');
-	var videoW = $('#my-video').width();
-	$('#my-video,.video-more').height(videoW/2);
+$(function(){
 	console.log(1)
-});
+})
