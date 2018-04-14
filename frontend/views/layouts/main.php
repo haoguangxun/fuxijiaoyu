09cce70851f791d51a0abb7ea4a4085c2fc0ad2b
jieery <lhshun2012@163.com>
2018-04-14 12:17:08 +0800

    nav

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -73,8 +73,12 @@ AppAsset::register($this);
             <li><a href="<?= Url::to(['default/index'])?>">首页</a></li>
             <li><a href="<?= Url::to(['about/index'])?>">学院概括</a></li>
             <li><a href="<?= Url::to(['teacher/list'])?>">师资力量</a></li>
-            <li><a href="<?= Url::to(['course/list','cid'=>15])?>">课程体系</a></li>
-            <li><a href="<?= Url::to(['course/list','cid'=>40])?>">面授课程</a></li>
+            <li>课程体系</a>
+            	<div class="twoNav">
+            		<a href="<?= Url::to(['course/list','cid'=>15])?>">在线课程</a>
+            		<a href="<?= Url::to(['course/list','cid'=>40])?>">面授课程</a>
+            	</div>
+            </li>
             <li><a href="<?= Url::to(['news/list','cid'=>64])?>">伏羲分院</a></li>
             <li><a href="<?= Url::to(['student/index'])?>">学员成长</a></li>
             <li><a href="<?= Url::to(['news/list','cid'=>1])?>">新闻动态</a></li>
